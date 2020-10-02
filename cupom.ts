c9c48755b798ce24870e0f9334469ed713a20ea8
Rodrigo Vilar <67280390+rodrigodcx@users.noreply.github.com>
2020-10-02 09:43:28 -0300

    Initial commit

---
+++ cupom.ts
@@ -0,0 +1,6 @@
+import { Loja } from './loja'; 
+
+export function dados_loja_objeto(loja: Loja) {
+  // Implemente aqui
+  return "";
+}
---
+++ loja.ts
@@ -0,0 +1,6 @@
+export class Loja {
+
+    constructor(public nome_loja: string, public logradouro: string, public numero: number, public complemento: string,
+        public bairro: string, public municipio: string, public estado: string, public cep: string,
+        public telefone: string, public observacao: string, public cnpj: string, public inscricao_estadual: string) { }
+}
